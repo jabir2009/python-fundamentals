67e13057fe1f1dffe99bc0c3490ff791bfb9d7b4
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-13 15:42:15 +0600

    learning python from scratch

---
+++ hello.py
@@ -0,0 +1,18 @@
+print('Hello Akib')
+
+print(1+5)
+
+print("I live in Sherpur")
+
+print('Hello Akib')
+
+print(1+5)
+
+print("I live in Sherpur")
+
+print('Hello Akib')
+
+print(1+5)
+
+print("I live in Sherpur")
+
---
+++ test.py
@@ -0,0 +1,11 @@
+number1 = 10
+
+number2 = 20
+
+result = number1 - number2
+
+print("The sum is:", result)
+
+akib_1 = "AKIB"
+
+print(akib_1)
