3fa0d5e2e5be2f254c8f141f91b7694fa9a9c44b
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-13 15:58:28 +0600

    Subtract Two Numbers

--- calculator.py
+++ calculator.py
@@ -3,25 +3,7 @@ number2 = int(input("enter the second integer:"))
 result = number1 + number2
 print(result)
 
-# number1=input("inter the first integer:")
-# number2=input("enter the second integer:")
-# result=number1-number2
-# print(result)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 - number2
+print(result)
