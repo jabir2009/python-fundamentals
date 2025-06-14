1725b8ec635faa3d673cd1c2d799ff66392a4e58
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-14 21:19:00 +0600

    add 2 calculation in calculator.py

--- calculator.py
+++ calculator.py
@@ -7,3 +7,43 @@ number1 = int(input("enter the first integer:"))
 number2 = int(input("enter the second integer:"))
 result = number1 - number2
 print(result)
+
+
+
+
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 * number2
+print(result)
+
+
+
+
+
+
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 / number2
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
