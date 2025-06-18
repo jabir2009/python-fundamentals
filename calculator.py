a3ca252726a1c4faf48d75f3f4d25a557c50d005
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-18 22:55:53 +0600

    modify calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,33 @@
-# 
+# number1 = int(input("enter the first integer:"))
+# number2 = int(input("enter the second integer:"))
+# result = number1 + number2
+# print(result)
+
+# number1 = int(input("enter the first integer:"))
+# number2 = int(input("enter the second integer:"))
+# result = number1 - number2
+# print(result)
+
+
+
+
+# number1 = int(input("enter the first integer:"))
+# number2 = int(input("enter the second integer:"))
+# result = number1 * number2
+# print(result)
+
+
+
+
+
+
+# number1 = int(input("enter the first integer:"))
+# number2 = int(input("enter the second integer:"))
+# result = number1 / number2
+# print(result)
+
+
+
 
 number1 = input ("enter a number between 1 - 3 :")
 number2 = input ("enter a number between 7 - 9 :")
@@ -6,11 +35,11 @@ number3 = input ("enter a number between 4 -6 :")
 print(" The big number is :" + number2 )
 
 
-
-
-
-
-
+number1 = input ("enter a number between 7 - 9 :")
+number2 = input ("enter a number between 1 - 2 :")
+number3 = input ("enter a number between 5 - 6 :")
+number4 = input ("enter a number between 3 - 4 :")
+print("The small number is :", number2 )
 
 
 
