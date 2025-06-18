f82b15d9cb2fee51b6104956be7419f9a38f8a0a
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-18 22:59:24 +0600

    modity calculator.py

--- calculator.py
+++ calculator.py
@@ -1,30 +1,30 @@
-# number1 = int(input("enter the first integer:"))
-# number2 = int(input("enter the second integer:"))
-# result = number1 + number2
-# print(result)
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 + number2
+print(result)
 
-# number1 = int(input("enter the first integer:"))
-# number2 = int(input("enter the second integer:"))
-# result = number1 - number2
-# print(result)
-
-
-
-
-# number1 = int(input("enter the first integer:"))
-# number2 = int(input("enter the second integer:"))
-# result = number1 * number2
-# print(result)
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 - number2
+print(result)
 
 
 
 
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 * number2
+print(result)
 
 
-# number1 = int(input("enter the first integer:"))
-# number2 = int(input("enter the second integer:"))
-# result = number1 / number2
-# print(result)
+
+
+
+
+number1 = int(input("enter the first integer:"))
+number2 = int(input("enter the second integer:"))
+result = number1 / number2
+print(result)
 
 
 
