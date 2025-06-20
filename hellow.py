7a5408e95b4a4d21c542cced75ac6f5b4d635f21
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-21 00:46:34 +0600

    modify hellow.py

--- hellow.py
+++ hellow.py
@@ -1,3 +1,17 @@
+from math import *
+
+
+print(max (968 , 7675 , 8766, 76 ))
+
+
+print(min (968 , 7675 , 8766, 76 ))
+
+number1 = input("enter first number :")
+number2 = input("enter second number :")
+number3 = input("enter third number :")
+number4 = input("enter forth number :")
+number5 = input("enter fifth number :")
+print(int(max(number1 , number2 , number3 , number4 , number5)))
 
 
 
@@ -7,26 +21,16 @@ number2 = input("enter second number :")
 number3 = input("enter third number :")
 number4 = input("enter forth number :")
 number5 = input("enter fifth number :")
-print(max("The big number is :", number1,number2,number3,number4,number5))
+print(min(number1,number2,number3,number4,number5))
 
 
 
-
-number1 = input("enter first number :")
-number2 = input("enter second number :")
-number3 = input("enter third number :")
-number4 = input("enter forth number :")
-number5 = input("enter fifth number :")
-print(min("The small number is :"+ number1,number2,number3,number4,number5))
-
-
-
-
-
-
-
-
-
+# number1 = input("enter first number :")
+# number2 = input("enter second number :")
+# number3 = input("enter third number :")
+# number4 = input("enter forth number :")
+# number5 = input("enter fifth number :")
+# print(max({"the big number is :"} , {number1} , {number2} , {number3} , {number4} , {number5}))
 
 
 
