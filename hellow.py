9639c5a3dab94f25251c2b0d281793da19667a72
jabir2009 <abdullah.jabir2009@gmail.com>
2025-06-20 23:51:08 +0600

    add two number analyze in hellow.py

---
+++ hellow.py
@@ -0,0 +1,43 @@
+
+
+
+
+number1 = input("enter first number :")
+number2 = input("enter second number :")
+number3 = input("enter third number :")
+number4 = input("enter forth number :")
+number5 = input("enter fifth number :")
+print(max("The big number is :", number1,number2,number3,number4,number5))
+
+
+
+
+number1 = input("enter first number :")
+number2 = input("enter second number :")
+number3 = input("enter third number :")
+number4 = input("enter forth number :")
+number5 = input("enter fifth number :")
+print(min("The small number is :"+ number1,number2,number3,number4,number5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
